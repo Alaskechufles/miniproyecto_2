29ade0134fad9005d6452552504e4c7200873713
Diego <alask27echufles@gmail.com>
2023-06-16 17:01:43 -0500

    segunda prueba con misnuscula

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<Alaskechufles>/<miniproyecto_2>.git main:gh-pages
+# git push -f git@github.com:<alaskechufles>/<miniproyecto_2>.git main:gh-pages
