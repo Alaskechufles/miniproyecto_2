dfc93f586457732badaaccb3c72f095d8cf40b99
Diego <alask27echufles@gmail.com>
2023-06-16 15:00:05 -0500

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "miniproyecto_2",
+  base: "/miniproyecto_2",
 });
